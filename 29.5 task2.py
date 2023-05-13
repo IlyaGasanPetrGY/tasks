840426797d5bba27a49ba75c376f08a409827433
ilyaDevops <ilya.gasan2020@mail.ru>
2023-05-14 00:55:57 +0300

    finished

---
+++ 29.5 task2.py
@@ -0,0 +1,53 @@
+
+# Функция принимает на вход строку, которая
+# состоит из скобок трех типов: круглые, квадратные
+# и фигурные. Функция должна проверить, является ли
+# переданная последовательность скобок сбалансированной
+# или нет. Функция возвращает True, если последовательность
+# сбалансирована, и False – в противном случае.
+def is_balanced(line):
+    arraypreteses = {'[': ']', '(': ')', '{': '}'}
+    stack = []
+    for i in line:
+        if i in list(arraypreteses.keys())[:3]:
+            stack.append(i)
+        else:
+            if arraypreteses[stack.pop()] != i:
+                return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+
+
+
+
+def test_is_balanced():
+    cases = {
+        '((((((((())))))))': False,
+        '{[()]}{{}}': True,
+        '{[()]}{]{}}': False,
+        '{{{((([[[]]])))}}}': True,
+        '{}': True,
+        '(': False,
+        '(}': False,
+        '(((())))[]{}': True,
+        '((()': False,
+        '[{}{})(]': False,
+        '{[]{([[[[[[]]]]]])}}': True,
+        '{[]{([[[[[[]])]]])}}': False,
+    }
+    for i, case in enumerate(cases.keys()):
+        if is_balanced(case) == cases[case]:
+            print(f'{i}: OK')
+        else:
+            print(f'{i}: Not OK')
+
+
+def main():
+    test_is_balanced()
+
+
+if __name__ == '__main__':
+    main()
